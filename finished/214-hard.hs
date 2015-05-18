fbfe2c33b6327bbe04dfd89ec8e39a459c147d95
werecat <werecat@hyperboria.ca>
2015-05-17 22:21:53 -0400

    Added some optimizations to 214-hard

--- finished/214-hard.hs
+++ finished/214-hard.hs
@@ -9,25 +9,30 @@ type Point = (Double, Double)
 type Points = V.Vector Point
 type Distance = Double
 
+-- Optimization to use less sqrt's
+distanceSquared :: Point -> Point -> Double
+distanceSquared (x1, y1) (x2, y2) = (x1 - x2)^2 + (y1 - y2)^2
+
+-- Not really used
 distance :: Point -> Point -> Double
-distance (x1, y1) (x2, y2) = sqrt $ (x1 - x2)^2 + (y1 - y2)^2
+distance pt1 pt2 = sqrt $ distanceSquared pt1 pt2
 
 -- Simple Brute Force Algorithm (There is probably a better method though)
 -- A more efficient algorithm would probably order the list in a better way
 totalDistanceBrute :: Distance -> Point -> Points -> Distance
 totalDistanceBrute travelled location pointList =
-    let distanceList = V.map (distance location) pointList
+    let distanceList = V.map (distanceSquared location) pointList
         minDistIndex = V.minIndex distanceList
-        minDistValue = distanceList V.! minDistIndex
+        minDistValue = sqrt $ distanceList V.! minDistIndex -- sqrt to calculate true distance
         minDistPoint = pointList V.! minDistIndex
-        -- change head and min value so I can just get tail
+        -- swap head and min value so I can remove by calling tail
         newPointList = pointList V.// [(minDistIndex, V.head pointList), (0, minDistPoint)]
         newTravelled = travelled + minDistValue
         -- Hey, they all have the same name length
         -- That's actually slightly confusing to look at
 
-        -- This if statement is here because I don't know a way to have check if a vector is empty
-        -- with an overloaded function like you can with lists (E.G. f a [] = a)
+        -- This if statement is here because I don't know a way to pattern match an empty vector
+        -- like you can with lists (E.G. f a [] = a)
     in  if (V.null $ V.tail newPointList)
             then newTravelled
             else totalDistanceBrute newTravelled minDistPoint $ V.tail newPointList
@@ -45,9 +50,9 @@ main = do
     progName <- getProgName
     file <- readFile $ head args
     if null args
-        then error $ "USAGE: " ++ progName ++ " pointListFile"
+        then error $ "USAGE: " ++ progName ++ " pointListFile.txt"
              -- This then doesn't every happen because the `file <- ...` before is apparently not lazy
-             -- and head args errors. Oh well, won't fix
+             -- and `head args` errors. Oh well, won't fix
         else let    pointList = pair . map read . tail . words $ file
                     -- tail is used to get rid of the number at the beginning of input, as it's not used
                     pointVec = V.fromList pointList
